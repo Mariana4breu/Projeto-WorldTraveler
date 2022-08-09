53fee421e62b1c81d2ea8a7acaa5b10a822c8537
Mariana Abreu <marianaabreuaffonso@gmail.com>
2022-08-09 15:45:37 -0300

    sobreComplete

--- menu.js
+++ menu.js
@@ -1,11 +1,3 @@
-/*const btnMenu = document.querySelector('.btn__menu');
-const menu = document.querySelector('.menu-lista');
-
-btnMenu.addEventListener('click', () =>{
-    menu.classList.toggle('menu-lista--ativo')
-})*/
-
-
 const btnMenu = document.getElementById('bt__menu');
 
 function toggleMenu() {
